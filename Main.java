7ad586dc29144d08380143aa12d3be434246263e
Wingemo <philip.wingemo@outlook.com>
2022-12-12 21:00:47 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,12 +9,6 @@ import java.net.Socket;
 
 public class Sender {
   public static void main(String[] args) {
-    Socket socket = new Socket("localhost", 8000);
-    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-    Person person = new Person("John", "Smith");
-    oos.writeObject(person);
-    oos.close();
-    socket.close();
-    
+   
   }
 }
