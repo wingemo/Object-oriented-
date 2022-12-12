75ae54b8f8a6c1a1fc1955cf574a8ae9be0bbac5
Wingemo <philip.wingemo@outlook.com>
2022-12-12 21:00:55 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,6 @@ Person person = client.receivePerson();
 System.out.println("Received person: " + person);
 client.close();
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 
 public class Sender {
   public static void main(String[] args) {
