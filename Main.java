36b7c7eef0696a6107397b3792574df15abc6399
Wingemo <philip.wingemo@outlook.com>
2022-12-12 21:04:19 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
-Client client = new Client();
-Person person = client.receivePerson();
-System.out.println("Received person: " + person);
-client.close();
-
-
-public class Sender {
+public class Main {
   public static void main(String[] args) {
-   
+    Server server = new Server();
+    Client client = new Client();
+    
+    Person person = client.receivePerson();
+    Person personToSend = new Person("John Doe");
+    server.sendPerson(personToSend);
+    System.out.println("Received person: " + person);
+    
+    client.close();
+    server.close();
   }
 }
