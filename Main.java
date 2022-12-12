8e8fd4d705bf214edbfc625da0733316f52b7f42
Wingemo <philip.wingemo@outlook.com>
2022-12-12 20:58:20 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+Client client = new Client();
+Person person = client.receivePerson();
+System.out.println("Received person: " + person);
+client.close();
+
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
