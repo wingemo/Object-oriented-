ba2d744db12d63dc022079aba8c2e091e34012ab
Wingemo <philip.wingemo@outlook.com>
2022-12-13 13:01:45 +0100

    Update Application.java

--- ObjectsOveraNetwork/Application.java
+++ ObjectsOveraNetwork/Application.java
@@ -11,19 +11,38 @@ public class Application {
    * @param args command-line arguments
    */
   public static void main(String[] args) {
+    String CONFIG_PATH = "config.properties";
+
     // Create a new server and client
+    Properties prop = readConfig(CONFIG_PATH);
     Server server = new Server();
     Client client = new Client();
-    
+
     // Create a new person and send it from the server to the client
     Person personToSend = new Person("John Doe");
     server.sendPerson(personToSend);
     Person person = client.receivePerson();
     System.out.println("Received person: " + person);
-    
+
     // Close the client and server connections
     client.close();
     server.close();
   }
-}
 
+  /**
+   * Reads the configuration properties from the specified file.
+   * 
+   * @param configPath the path to the configuration file
+   * @return a {@code Properties} object containing the configuration properties
+   * @throws IOException if an error occurs while reading the file
+   */
+  public static Properties readConfig(String configPath) {
+    try (InputStream input = new FileInputStream(configPath)) {
+      Properties prop = new Properties();
+      prop.load(input);
+      return prop;
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+}
